111564ba3afd5174f9903d88d2fdf3f94d5df6af
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-06-01 17:27:29 +0530

    min distance

--- 32_Binary Trees 3/Lca.java
+++ 32_Binary Trees 3/Lca.java
@@ -63,6 +63,34 @@ public class Lca {
         return root;
     }
 
+    public static int lcaDist(Node root, int n) {
+        if(root == null) {
+            return -1;
+        }
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if(leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -72,9 +100,10 @@ public class Lca {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 4, n2 = 5;
+        int n1 = 4, n2 = 6;
         System.out.println(lca(root, n1, n2).data);
         System.out.println(lca2(root, n1, n2).data);
+        System.out.println(minDist(root, n1, n2));
     }
     
 }
